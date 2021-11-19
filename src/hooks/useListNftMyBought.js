51f284154bbb890e1eb00d816999310eda4ce9d9
hieutt <hieutt@vnext.com.vn>
2021-11-19 18:47:14 +0700

    Update my nft page

--- src/hooks/useListNftMyBought.js
+++ src/hooks/useListNftMyBought.js
@@ -6,6 +6,7 @@ import _ from 'lodash'
 import { useDispatch, useSelector } from 'react-redux'
 import useBlock from './useBlock'
 import { fetchMetadata } from '../states/tokenDataSlice'
+import { jsx } from '@emotion/react'
 
 const useListNftMyBought = () => {
   const nftContract = useNtfContract()
@@ -15,63 +16,29 @@ const useListNftMyBought = () => {
   const block = useBlock()
   const dispatch = useDispatch()
   const metaData = useSelector((state) => state.tokenData.metaData)
-
+  const fetchUserNft = async() => {
+    const markets = await axios.get(`${process.env.REACT_APP_API_URL}/nfts/user/${account}`)
+    console.log('markets::>>', markets)
+     const data = markets.data.nfts.map(marketItem => {
+      let item = {
+        tokenId: marketItem.tokenId,
+        image: marketItem.img,
+        class: marketItem.class,
+        level: marketItem.level.toString(),
+        heath: marketItem.heath.toString(),
+        morale: marketItem.morale.toString(),
+        skill: marketItem.skill.toString(),
+        speed: marketItem.speed.toString(),
+        buyer: marketItem.buyer? marketItem.buyer : ''
+      }
+      return item
+    })
+    setList(data)
+  } 
   useEffect(() => {
     ;(async () => {
       if (nftContract && nftMarketContract && account) {
-        // get list tokens
-        const listMyBoughtItems = await nftMarketContract.fetchMyNFTs(account)
-        const listMySellItems = await nftMarketContract.fetchItemsCreated(account)
-        //
-        const sortedListMyBoughtItems = _.orderBy(listMyBoughtItems, ['itemId'], ['desc'])
-        const uniqueListMyBoughtItems = _.uniqBy(sortedListMyBoughtItems, 'tokenId')
-        //
-        const sortedListMySellItems = _.orderBy(listMySellItems, ['itemId'], ['desc'])
-        const uniqueListMySellItems = _.uniqBy(sortedListMySellItems, 'tokenId')
-
-        const myItem = uniqueListMyBoughtItems.filter((buyItem) => {
-          const buyTokenId = buyItem.tokenId.toNumber()
-          const buyId = buyItem.itemId.toNumber()
-          const sellItems = uniqueListMySellItems.filter((sellItem) => {
-            const sellTokenId = sellItem.tokenId.toNumber()
-            const sellId = sellItem.itemId.toNumber()
-            if (sellTokenId === buyTokenId && sellId > buyId) {
-              return true
-            }
-            return false
-          })
-          if (!sellItems.length) {
-            return true
-          }
-          return false
-        })
-        const data = await Promise.all(
-          myItem.map(async (i) => {
-            let meta
-            if (!metaData[i.tokenId]) {
-              const tokenUri = await nftContract.tokenURI(i.tokenId)
-              meta = (await axios.get(tokenUri)).data
-              dispatch(fetchMetadata({ meta: meta, id: i.tokenId }))
-            }else{
-              meta = metaData[i.tokenId]
-            }
-            const tokenState = await nftContract.tokenDetails(i.tokenId)
-            let item = {
-              id: i.itemId.toString(),
-              tokenId: i.tokenId.toNumber(),
-              image: meta.urlImage,
-              class: meta.classId,
-              level: tokenState.level.toString(),
-              heath: tokenState.heath.toString(),
-              morale: tokenState.morale.toString(),
-              skill: tokenState.skill.toString(),
-              speed: tokenState.speed.toString(),
-              buyer: i.buyer
-            }
-            return item
-          }),
-        )
-        setList(data)
+        await fetchUserNft()
         return true
       }
     })()
